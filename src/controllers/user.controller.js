30a63fc8fe30407a396c0beb675735ca1060fe22
bipulkushwaha <bipulkushwaha58@gmail.com>
2024-02-03 15:47:15 +0530

    login logout controller generating access and refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/FileUpload.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken(); //here also generateAccessToken and generateRefreshToken is our own method which is attached to userSchema so it will acccessed by user, which we are getting from db and this user is user instance. own method can't be accessed by User which is mongoose refrence.
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access and refresh token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { username, fullName, email, password } = req.body;
 
@@ -22,11 +39,17 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "user already Exist");
   }
 
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;   // coverimnage will not come from UI then it will be undefine.
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  let coverImageLocalPath
 
-  if(req.files?.coverImage[0]?.path){
-    coverImageLocalPath = req.files?.coverImage[0]?.path
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files?.coverImage[0]?.path;
   }
 
   if (!avatarLocalPath) {
@@ -43,13 +66,15 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     fullName,
     avatar: avatar.url,
-    coverImage: coverImage?.url || "",
+    coverImage: coverImage?.url || "", // here if cover image is not there then it will will be undefine and undefine.url will break the code.
     email,
     password,
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
   if (!createdUser) {
     throw new ApiError(500, "something went wrong while registering the user");
@@ -60,7 +85,84 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!username && !email) {
+    throw new ApiError(400, "username or password is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username, email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "user does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password); //we have added our own isPasswordCorrect method to userSchema so it will available in user which we are getting from db, not in User(this is mongoose User)
+
+  if (!isPasswordValid) {
+    throw new ApiError(404, "Invalid credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  ); //here instead of db call to find updated user instance we can update user with accessToken and refresh token
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User loggedIn successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out Successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
+
+// REGISTER STEPS BELOW:-->
 
 // get user details from frontend
 // validate - not empty
@@ -71,3 +173,12 @@ export { registerUser };
 // remove password and refresh token field from response
 // check user creation
 // return response
+
+// LOGIN STEPS BELOW:-->
+
+//get data from req body
+//username or email
+//find user
+//password check
+//generate access and refresh token
+//send access and refresh token in cookie
