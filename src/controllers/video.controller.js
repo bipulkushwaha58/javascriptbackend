6828888d4d64b36018590d6c9c53c6127731ab08
bipulkushwaha <bipulkushwaha58@gmail.com>
2024-02-09 23:23:19 +0530

    video publish controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,6 +5,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/FileUpload.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
@@ -14,6 +15,31 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
   // TODO: get video, upload to cloudinary, create video
+  const videoFileLocalPath = req?.files.videoFile[0].path;
+  const thumbnailLoaclPath = req?.files.thumbnail[0].path;
+  if (!videoFileLocalPath) {
+    throw new ApiError(404, "Video is required");
+  }
+  if (!thumbnailLoaclPath) {
+    throw new ApiError(404, "Video is required");
+  }
+
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLoaclPath);
+  const owner = req.user; //it will come from verifyJWT middleware.
+
+  const video = await Video.create({
+    videoFile: videoFile.url,
+    thumbnail: thumbnail.url,
+    title,
+    description,
+    duration: videoFile.duration,
+    owner,
+  });
+   //no need to find video in db, if there will any error while creating video document in db it should trow error, in case of error below code will not execute.
+return res
+  .status(201)
+.json(new ApiResponse(201,video, "video uploaded successfully"))
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
