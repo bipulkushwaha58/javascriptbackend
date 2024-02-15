0dfae8a2b8b79bdf1a1189967c34aa3f6e0bb54b
bipulkushwaha <bipulkushwaha58@gmail.com>
2024-02-16 02:38:19 +0530

    toggle subscription controller done

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,20 +7,60 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  const { id: subscriberId } = req.user;
+  const { _id: subscriberId } = req.user;
   // TODO: toggle subscription
-  
+
   try {
-    // const isSubscribed = await Subscription.find(
-    //     {
-    //         "channel._id": channelId,               // Since i need to query more than one collection, i can use MongoDB's aggregation.
-    //         "subscriber._id": subscriberId
-    //     }
-    //   )
-    //   console.log("isSubscribed <<=======", isSubscribed)
-    //   if(isSubscribed){
-    //     throw new ApiError(400,"already subscribed")
-    //   }
+    const userSubscriptionDocument = await User.aggregate([              //Since you need to query more than one collection, you can use MongoDB's aggregation.
+      {
+        $match: { _id: new mongoose.Types.ObjectId(req.user._id) },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo",
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers",
+        },
+      },
+      {
+        $addFields: {
+          // subscribedTo: {
+          //   $arrayElemAt: ["$subscribedTo",0]
+          // },
+          // subscribers: {
+          //   $arrayElemAt:["$subscribers",0]
+          // },
+          subscriberCount: {
+            $size: "$subscribers",
+          },
+          isSubscribed: {
+            $cond: {
+              if: {
+                $in: [
+                  new mongoose.Types.ObjectId(channelId),
+                  "$subscribedTo.channel",
+                ],
+              },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+    ]);
+
+    if (userSubscriptionDocument[0].isSubscribed) {
+      throw new ApiError(400, "already subscribed");
+    }
     const subscriber = await User.findById(subscriberId);
     const channel = await User.findById(channelId);
     if (!(subscriber && channel)) {
@@ -29,17 +69,20 @@ const toggleSubscription = asyncHandler(async (req, res) => {
         "something went wrong while fetching subscriber or channel"
       );
     }
-    const subscription = await Subscription.create(
-        {
-            subscriber,
-            channel
-        }
-    )
+    const subscription = await Subscription.create({
+      subscriber,
+      channel,
+    });
 
     return res
-            .status(201)
-            .json(new ApiResponse(201,subscription,"subscription document created successfully"))
-
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          subscription,
+          "subscription document created successfully"
+        )
+      );
   } catch (error) {
     throw error;
   }
@@ -48,6 +91,28 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  const subscriberListOfChannel = await Subscription.find( {channel: {_id : new mongoose.Types.ObjectId(channelId)}})
+
+  // const subscriberListOfChannel = await Subscription.aggregate(
+  //   [
+  //     {
+  //       $match: { channel: new mongoose.Types.ObjectId(channelId) },
+  //     },
+  //     {
+  //       $lookup:{
+  //         from: "users",
+  //         localField: "channel",
+  //         foreignField: "_id",
+  //         as: "subscribers"
+  //       }
+  //     }
+  //   ]
+  // )
+
+  console.log("subscriberListOfChannel======>>", subscriberListOfChannel)
+
+
+
 });
 
 // controller to return channel list to which user has subscribed
